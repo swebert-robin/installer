0c9b5e5776d92c3da70bdcaf787ac8c0bf45f116
Swebert Correa <swebert@robin.io>
2023-09-23 00:23:02 +0530

    Create object-store-installer.sh

---
+++ object-store-installer.sh
@@ -0,0 +1,17 @@
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf aws awscliv2.zip
+
+tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+
+yum install -y libxcrypt-compat.x86_64
+yum install -y google-cloud-cli
